b33040f88c2fc6ad5461c99711477f4d40fc6bb9
Brandon Chinn <brandonchinn178@gmail.com>
2023-11-28 21:19:48 -0800

    Continue implementation

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ document.addEventListener('alpine:init', () => {
 class PresenterState {
   /***** Loading/saving *****/
 
+  loadFromData(data) {
+    this.phase = data.phase || 'setup'
+    this.players = data.players || []
+    this.board = data.board || []
+  }
+
   static loadFromStorage() {
     if (!window.localStorage) {
       window.alert("Your browser does not support local storage. Your changes will not be saved.")
@@ -29,29 +35,92 @@ class PresenterState {
     window.localStorage.setItem("presenter-state", JSON.stringify(this))
   }
 
-  loadFromData(data) {
-    this.phase = data.phase || 'setup'
-    this.players = data.players || []
-  }
-
   reset() {
+    if (!confirm('This will clear all the data. Are you sure?')) return
+
     this.loadFromData({})
   }
 
-  /***** Setup phase *****/
+  /***** Phase controllers *****/
 
-  endSetup() {
-    this.requirePhase('setup')
-    this.phase = 'game'
+  get setup() {
+    this.expectPhase('setup')
+    return new SetupController(this)
   }
 
-  /***** Game phase *****/
+  get game() {
+    this.expectPhase('game')
+    return new GameController(this)
+  }
 
-  /***** Helpers *****/
-
-  requirePhase(expected) {
+  expectPhase(expected) {
     if (this.phase !== expected) {
       throw new Error(`Expected phase: ${expected}, got: ${this.phase}`)
     }
   }
+}
+
+class SetupController {
+  constructor(state) {
+    this.state = state
+  }
+
+  get players() {
+    return this.state.players
+  }
+
+  addPlayer(player) {
+    if (this.state.players.includes(player)) {
+      return
+    }
+
+    this.state.players.push(player)
+  }
+
+  removePlayer(player) {
+    this.state.players = this.state.players.filter((p) => p !== player)
+  }
+
+  startGame() {
+    this.state.phase = 'game'
+    this.state.game.init()
+  }
+}
+
+class GameController {
+  constructor(state) {
+    this.state = state
+  }
+
+  get board() {
+    return this.state.board
+  }
+
+  get isDone() {
+    return this.currentPlayer === null
+  }
+
+  get currentPlayer() {
+    // TODO: handle first player at end
+    const player = this.state.board.find(({ gift }) => gift === null)
+    return player ? player.name : null
+  }
+
+  init() {
+    this.state.board =
+      shuffleArray(this.state.players).map(
+        (player) => ({ name: player, gift: null })
+      )
+  }
+}
+
+/**
+ * Return a new array containing the elements in the given array shuffled randomly.
+ * https://stackoverflow.com/a/46545530
+ */
+function shuffleArray(arr) {
+  return arr
+    .map((value) => ({ value, key: Math.random() }))
+    .sort((a, b) => a.key - b.key)
+    .map(({ value }) => value)
 }
